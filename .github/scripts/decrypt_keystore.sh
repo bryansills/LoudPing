f55f3bab21c86b513cb5347f631448f0db9ada38
bryansills <bryan@bryansills.com>
2024-10-31 22:03:10 -0500

    Let's see if I broke Github Actions...

--- .github/scripts/decrypt_keystore.sh
+++ .github/scripts/decrypt_keystore.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$KEYSTORE_GPG_PASSPHRASE" \
+gpg --quiet --batch --yes --decrypt --passphrase="$1" \
 --output release/loudping-upload.jks release/loudping-upload.jks.gpg
