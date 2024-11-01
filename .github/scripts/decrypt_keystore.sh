7983706774260d0b384cf7d1bf18a123fffd03a6
bryansills <bryan@bryansills.com>
2024-10-31 22:29:10 -0500

    Fully argumentize stuff...

--- .github/scripts/decrypt_keystore.sh
+++ .github/scripts/decrypt_keystore.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 # --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$1" \
---output release/loudping-upload.jks release/loudping-upload.jks.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$1" --output $2 $3
