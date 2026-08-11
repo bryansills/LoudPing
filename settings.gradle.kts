741b6017551c43b3cf895d8aa8395f7fc35a0bf6
bryansills <bryan@bryansills.com>
2026-08-10 19:05:59 -0500

    Move Gradle plugin stuff into settings.gradle.kts and add dedebug plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,24 @@ buildscript {
 }
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("com.autonomousapps.build-health") version "3.18.0"
+    id("com.autonomousapps.dedebug") version "0.1"
+    id("com.android.application") version "9.3.1" apply false
+    id("com.android.library") version "9.3.1" apply false
+    id("com.android.kotlin.multiplatform.library") version "9.3.1" apply false
+    id("com.github.gmazzo.buildconfig") version "6.0.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
+    id("org.jetbrains.compose") version "1.11.1" apply false
+    id("com.dropbox.dependency-guard") version "0.5.0" apply false
+    id("com.google.dagger.hilt.android") version "2.60.1" apply false
+    id("org.jetbrains.kotlin.jvm") version "2.4.10" apply false
+    id("org.jetbrains.kotlin.multiplatform") version "2.4.10" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.4.10" apply false
+    id("com.google.devtools.ksp") version "2.3.11" apply false
+    id("org.jetbrains.kotlin.plugin.parcelize") version "2.4.10" apply false
+    id("app.cash.sqldelight") version "2.3.2" apply false
+    id("com.bugsnag.gradle") version "1.2.0" apply false
+    id("app.cash.burst") version "2.13.0" apply false
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
