edcfb5c2398090d76b9091b45559ab76c05a1f6b
bryansills <bryan@bryansills.com>
2026-09-26 13:29:59 -0500

    Keep all the AGPs in line

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,8 +17,8 @@ plugins {
     id("com.autonomousapps.build-health") version "3.19.1"
     id("com.autonomousapps.dedebug") version "0.1"
     id("com.android.application") version "9.4.1" apply false
-    id("com.android.library") version "9.4.0" apply false
-    id("com.android.kotlin.multiplatform.library") version "9.3.2" apply false
+    id("com.android.library") version "9.4.1" apply false
+    id("com.android.kotlin.multiplatform.library") version "9.4.1" apply false
     id("com.github.gmazzo.buildconfig") version "6.1.1" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.20" apply false
     id("org.jetbrains.compose") version "1.12.0" apply false
